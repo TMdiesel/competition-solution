8f563a9d6aca4a9470f1a3b553ecf8b12805fa16
TMdiesel <cerezzodora2262@icloud.com>
2021-11-04 14:41:30 +0000

    feat: add template script

--- kaggle-rfcx/exp/exp00_stage1.py
+++ kaggle-rfcx/exp/exp00_stage1.py
@@ -1,1 +1,90 @@
-import numpy
+# default package
+import logging
+import pathlib
+import sys
+import os
+
+# third party package
+import numpy as np
+import pandas as pd
+import torch
+
+if "ipykernel" in sys.modules:
+    from tqdm.notebook import tqdm
+else:
+    from tqdm import tqdm
+
+from dotenv import load_dotenv
+
+# my package
+
+# global variable
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+_logger = logging.getLogger(__name__)
+if "KAGGLE_URL_BASE" in set(os.environ.keys()):
+    from kaggle_secrets import UserSecretsClient
+
+    ENV = "KAGGLE"
+    USER_SECRETS = UserSecretsClient()
+elif "google.colab" in sys.modules:
+    ENV = "COLAB"
+else:
+    path = (pathlib.Path(os.path.dirname(__file__))).joinpath("../.env")
+    load_dotenv(path)
+    ENV = "LOCAL"
+
+
+# environment variable
+def load_env(label: str):
+    if ENV in ["KAGGLE"]:
+        return USER_SECRETS.get_secret(label)
+    elif ENV in ["LOCAL", "COLAB"]:
+        return os.environ.get(label)
+
+
+WANDB_API = load_env("WANDB_API")
+OUTPUT_DIR = load_env("OUTPUT_DIR")
+INPUT_DIR = load_env("INPUT_DIR")
+
+
+# =================================================
+# Config
+# =================================================
+class CFG:
+    debug = False
+    log_dir = "./kaggle-rfcx/logs"
+
+
+# =================================================
+# Utils
+# =================================================
+def init_root_logger(
+    outdir: pathlib.Path,
+    filename_normal: str = "log.log",
+    filename_error: str = "error.log",
+):
+
+    outdir.mkdir(exist_ok=True)
+    logging.getLogger().addHandler(_add_handler(outdir, logging.INFO, filename_normal))
+    logging.getLogger().addHandler(_add_handler(outdir, logging.ERROR, filename_error))
+
+
+def _add_handler(outdir: pathlib.Path, level, filename):
+    fh = logging.FileHandler(outdir / filename)
+    fh.setLevel(level)
+    fh_formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(filename)s - %(name)s - %(funcName)s - %(message)s"
+    )
+    fh.setFormatter(fh_formatter)
+    return fh
+
+
+def main():
+    init_root_logger(pathlib.Path(CFG.log_dir))
+    _logger.setLevel(logging.INFO)
+
+    _logger.info(ENV)
+
+
+if __name__ == "__main__":
+    main()
